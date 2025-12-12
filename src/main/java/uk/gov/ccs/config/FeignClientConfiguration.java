54763f4313e63d6916f76cb0c66f8a1423aac924
Tom <60753515+tberey@users.noreply.github.com>
2025-12-12 15:36:30 +0000

    Update FeignClientConfiguration.java

--- src/main/java/uk/gov/ccs/config/FeignClientConfiguration.java
+++ src/main/java/uk/gov/ccs/config/FeignClientConfiguration.java
@@ -3,6 +3,8 @@ package uk.gov.ccs.config;
 import feign.Logger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
 
 /**
  *  This is the temporary Configuration object to print full log for Feign client.
@@ -25,5 +27,16 @@ public class FeignClientConfiguration {
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL;
+    }
+
+    @Bean
+    public RequestInterceptor feignRequestInterceptor() {
+        return new RequestInterceptor() {
+            @Override
+            public void apply(RequestTemplate template) {
+                System.out.println("Feign OUTBOUND METHOD: " + template.method());
+                System.out.println("Feign OUTBOUND URL: " + template.url());
+            }
+        };
     }
 }
