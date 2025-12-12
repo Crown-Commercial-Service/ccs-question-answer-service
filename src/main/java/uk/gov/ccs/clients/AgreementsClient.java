c0db60b8656226dc6023e269e96f555f0192e8a1
Tom <60753515+tberey@users.noreply.github.com>
2025-12-12 15:46:26 +0000

    Update AgreementsClient.java

--- src/main/java/uk/gov/ccs/clients/AgreementsClient.java
+++ src/main/java/uk/gov/ccs/clients/AgreementsClient.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import uk.gov.ccs.model.agreements.*;
+import uk.gov.ccs.config.FeignClientConfiguration;
 
 import java.util.Collection;
 import java.util.List;
